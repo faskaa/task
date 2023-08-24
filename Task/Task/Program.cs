694d7982018cead0048efa6bb6b65e91cd3df762
faskaa <fazilnb@code.edu.az>
2023-08-24 16:56:01 +0400

    commit initizailed

---
+++ Task/Task/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Task
+{
+   public class NotAnEmailAdressException : Exception
+   {
+        public NotAnEmailAdressException() { }
+        public NotAnEmailAdressException(string name) : base(name)
+        {
+            
+        }
+    }
+    public class ArgumentOutOfRangeException : Exception
+    {
+        public ArgumentOutOfRangeException() { }
+        public ArgumentOutOfRangeException(string name) : base(name)
+        {
+
+        }
+    }
+
+
+
+    public class EmailValidator
+    {
+        public bool Validate (string email)
+        {
+            if (!email.EndsWith("mail.com"))
+            {
+                throw new NotAnEmailAdressException("Error");
+            }
+            if (email.Length > 9)
+            {
+                throw new ArgumentOutOfRangeException("Error");
+            }
+            return true;
+           
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var emailvalidator = new EmailValidator();
+            
+
+            try
+            {
+                emailvalidator.Validate("fazilbehramov@maill.ru");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+            
+            
+        }
+    }
+}
